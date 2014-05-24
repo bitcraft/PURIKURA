b5cb361c03014303371cf966e607176a48a76adf
Leif Theden <leif.theden@gmail.com>
2014-05-24 00:13:26 -0500

    templating

--- pyrikura/plugins/composer/filters.py
+++ pyrikura/plugins/composer/filters.py
@@ -61,7 +61,7 @@ def toaster(filename, w, h, output=None):
     scratch = colortone(filename, '#110000', 105, 0, output=scratch)
 
     # contrast
-    cmd = 'convert {} -modulate 200,110,100 {}'.format(
+    cmd = 'convert {} -modulate 300,110,100 {}'.format(
         scratch, scratch
     )
 
