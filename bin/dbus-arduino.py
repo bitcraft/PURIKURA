0c8271f026aeb8c972207099a903249aa71b521a
bitcraft <leif.theden@gmail.com>
2014-07-11 11:21:30 -0500

    dbus madness

--- bin/dbus-arduino.py
+++ bin/dbus-arduino.py
@@ -29,7 +29,7 @@ bus = dbus.SessionBus()
 
 class EmitterObject(dbus.service.Object):
     def __init__(self):
-        dbus.service.Object.__init__(self, bus, 'com.kilbuckcreek.photobooth')
+        dbus.service.Object.__init__(self, bus, '/com/kilbuckcreek/photobooth')
 
     @dbus.service.signal(dbus_interface='com.kilbuckcreek.photobooth')
     def emit(self):
